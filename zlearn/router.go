440c1cab36700b24bc3e9f967f19f53a186efe4d
hasmorebug <hasbug@sina.com>
2018-06-25 13:59:00 +0800

    httprouter example2

--- zlearn/router.go
+++ zlearn/router.go
@@ -9,16 +9,71 @@ import (
 
 /////////
 func HttpRouterExample() {
-	hrExmaple1()
+	hrExample3()
+	//hrExample2()
+	//hrExmaple1()
+}
+
+///////////
+// Basic Authentication
+func BasicAuth(h httprouter.Handle, requiredUser, requiredPassword string) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		user, password, hasAuth := r.BasicAuth()
+
+		if hasAuth && user == requiredUser && password == requiredPassword {
+			h(w, r, ps)
+		} else {
+			w.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		}
+	}
+}
+
+func Protect(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprint(w, "Protected!\n")
+}
+
+func hrExample3() {
+	user := "gordon"
+	pass := "secret!"
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/protected/", BasicAuth(Protect, user, pass))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+//////////
+// Multi-domain/Sub-domain
+type HostSwitch map[string]http.Handler
+
+func (hs HostSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if handler := hs[r.Host]; handler != nil {
+		handler.ServeHTTP(w, r)
+	} else {
+		http.Error(w, "Forbidden", 403)
+	}
+}
+
+func hrExample2() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	hs := make(HostSwitch)
+	hs["localhost:8080"] = router
+
+	log.Fatal(http.ListenAndServe(":8080", hs))
 }
 
 /////////
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+	fmt.Fprint(w, "Not protected!\n")
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprint(w, "Protected!\n")
 }
 
 func hrExmaple1() {
